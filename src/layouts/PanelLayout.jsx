7576318cbe9a5b0a1c6e22d8857a83b03fdb52cf
Erfan gharche beydokhti <erfan.erato.79@gmail.com>
2023-02-10 10:02:12 +0330

    create card side slider

--- src/layouts/PanelLayout.jsx
+++ src/layouts/PanelLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Object from "../assets/sidebar-pic-1.png";
 import SearcheBar from "../components/SearchBar";
+import { TbShoppingCart } from "react-icons/tb";
 
 const PanelLayout = ({ children }) => {
   return (
@@ -75,7 +76,7 @@ const PanelLayout = ({ children }) => {
                   <div className="row">
                     <div className="col">
                       <div className="first-card-side-slider">
-                        <div className="container">
+                        <div className="container first-card-container">
                           <div className="first-card-line row">
                             <div className="col first-section-of-line-one">
                               Sales Overview
@@ -87,10 +88,36 @@ const PanelLayout = ({ children }) => {
                           <div className="row card-price">$42.5k</div>
                           <div className="row">
                             <div className="col-5 order-section">
-                              <div className="order-icon"></div>
+                              <div className="order-icon">
+                                <div className="shopping-icon">
+                                  <TbShoppingCart
+                                    className="main-shopping-icon"
+                                    size="20px"
+                                    color="#03b4bc"
+                                  />
+                                </div>
+                                <div className="order-text">Order</div>
+                              </div>
+                              <div className="number-1">62.2%</div>
+                              <div className="number-2">6,440</div>
                             </div>
-                            <div className="col-2"></div>
-                            <div className="col-5"></div>
+
+                            <div className="col-2 vs-betwwen">Vs</div>
+
+                            <div className="col-5 visit-section">
+                              <div className="visit-icon">
+                                <div className="visit-text">Visits</div>
+                                <div className="link-icon">
+                                  <TbShoppingCart
+                                    className="main-shopping-icon"
+                                    size="20px"
+                                    color="#685fca"
+                                  />
+                                </div>
+                              </div>
+                              <div className="number-3">25.5%</div>
+                              <div className="number-4">12,749</div>
+                            </div>
                           </div>
                         </div>
                       </div>
