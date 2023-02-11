578591171741c73dd70b728fd6aec18048130a73
Erfan gharche beydokhti <erfan.erato.79@gmail.com>
2023-02-11 12:28:46 +0330

    remove margin from cols

--- src/components/WebAnalytic.jsx
+++ src/components/WebAnalytic.jsx
@@ -4,16 +4,14 @@ import { BsLink45Deg } from "react-icons/bs";
 import ProgressBar from "../components/ProgressBar";
 import Object from "../assets/sidebar-pic-1.png";
 
-
-
 const WebAnalytics = () => {
   return (
     <div className="web-analytic">
       <div className="row web-analytic-main-row">
-        <div className="col">
+        <div className="col-6">
           <div className="slider-div-holder">
             <div className="row">
-              <div className="col text-side-object-col-holder">
+              <div className="col-8 text-side-object-col-holder">
                 <div className="text-side-object">
                   <div className="main-text-holder-side-object">
                     <div className="Analytics-text">Website Analytics</div>
@@ -24,28 +22,28 @@ const WebAnalytics = () => {
 
                     <div className="row-down-slider">
                       <div className="row">
-                        <div className="col">
+                        <div className="col-3">
                           <div className="first-section">
                             <div className="h">55%</div>
                             <div>Sessions</div>
                           </div>
                         </div>
-                        <div className="col">
-                          <div className="first-section">
+                        <div className="col-3">
+                          <div className="first-section2">
                             <div className="h">12h</div>
-                            <div>Order Size</div>
+                            <div>Order</div>
                           </div>
                         </div>
                       </div>
                       <div className="row second-row">
-                        <div className="col">
-                          <div className="first-section">
+                        <div className="col-3">
+                          <div className="first-section3">
                             <div className="h">145k</div>
                             <div>Leads</div>
                           </div>
                         </div>
-                        <div className="col">
-                          <div className="first-section">
+                        <div className="col-3">
+                          <div className="first-section4">
                             <div className="h">240</div>
                             <div>Campaign</div>
                           </div>
@@ -55,7 +53,7 @@ const WebAnalytics = () => {
                   </div>
                 </div>
               </div>
-              <div className="col text-side-object">
+              <div className="col-4 text-side-object">
                 <div className="object-image">
                   <img src={Object} className="main-object-image" />
                 </div>
@@ -64,18 +62,20 @@ const WebAnalytics = () => {
           </div>
         </div>
 
-        <div className="col">
+        <div className="col-6">
           <div className="row">
-            <div className="col">
+            <div className="col-6">
               <div className="first-card-side-slider">
                 <div className="container first-card-container">
                   <div className="first-card-line row">
-                    <div className="col first-section-of-line-one">
+                    <div className="col-6 first-section-of-line-one">
                       Sales Overview
                     </div>
-                    <div className="col second-section-of-line-one">+18.2%</div>
+                    <div className="col-6">
+                      <div className="second-section-of-line-one">+18.2%</div>
+                    </div>
                   </div>
-                  <div className="row card-price">$42.5k</div>
+                  <div className="row "><div className="col-12"><div className="card-price">$42.5k</div></div></div>
                   <div className="row">
                     <div className="col-5 order-section">
                       <div className="order-icon">
@@ -111,16 +111,17 @@ const WebAnalytics = () => {
                   </div>
 
                   <div>
-                    {" "}
-                    <ProgressBar done="70" />{" "}
+                    <ProgressBar done="70" />
                   </div>
+                  
                 </div>
               </div>
             </div>
 
-            <div className="col">
+            <div className="col-6">
               <div className="second-card-side-slider">x2</div>
             </div>
+
           </div>
         </div>
       </div>
