e9aa411f033163331fe78230527af0832cfcba74
Erfan gharche beydokhti <erfan.erato.79@gmail.com>
2023-02-14 13:15:24 +0330

    complete email

--- src/components/EmailForms.jsx
+++ src/components/EmailForms.jsx
@@ -5,6 +5,11 @@ import { TbPencil } from "react-icons/tb";
 import { BsStar } from "react-icons/bs";
 import { RiErrorWarningLine } from "react-icons/ri";
 import { HiOutlineTrash } from "react-icons/hi";
+import { AiOutlineSearch } from "react-icons/ai";
+import { VscRefresh } from "react-icons/vsc";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { ImStarEmpty } from "react-icons/im";
+import profile from "../assets/avatar-1.129659bb.png";
 import "../style/email-page-style.scss";
 
 const EmailForms = () => {
@@ -12,7 +17,7 @@ const EmailForms = () => {
     <div className="background-email-form">
       <div className="main-email-form">
         <div className="row">
-          <div className="col-sm-3 col-md-3 border-between">
+          <div className="col-3 col-sm-3 col-md-3">
             <div className="email-side-bar">
               <div className="container sidebar-holder">
                 <div className="compose-button">
@@ -138,9 +143,88 @@ const EmailForms = () => {
             </div>
           </div>
 
-          <div className="col-sm-9 col-md-9">
+          <div className="col-9 col-sm-9 col-md-9">
             <div className="email-content">
-              <div>asdasdasdasd</div>
+              <div className="xxxxxx">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="search-icon-holder">
+                      <AiOutlineSearch size="27px" color="#6d7286" />
+                      <input
+                        className="search-text"
+                        placeholder="Search email"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="first-row-holder">
+                    <div className="row">
+                      <div className="col-sm-6 col-md-6">
+                        <div className="check-box">
+                          <div className="main-check-box"></div>
+                        </div>
+                      </div>
+
+                      <div className="col-sm-6 col-md-6">
+                        <div className="refresh-icon ">
+                          <VscRefresh
+                            size="20px"
+                            color="#6d7286"
+                            className="main-refresh-icon"
+                          />
+                          <BsThreeDotsVertical
+                            size="20px"
+                            color="#6d7286"
+                            className="main-thre-icon"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="first-row-holder">
+                    <div className="row">
+                      <div className="col-sm-6 col-md-6">
+                        <div className="check-box">
+                          <div className="main-check-box"></div>
+                          <div className="star-holder">
+                            <ImStarEmpty
+                              size="23px"
+                              color="#6d7286"
+                              className="star"
+                            />
+                          </div>
+
+                          <div class="avatar-image">
+                            <img src={profile} class="avatar" />
+                          </div>
+
+                          <div className="email-title">
+                            Charles Cooper Fight account night short.
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="col-sm-6 col-md-6">
+                        <div className="date-importance">
+                          <div className="importance">
+                            <div className="label-importance"></div>
+                          </div>
+                          <div className="date">jul22</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
