8eb942248f817183846adb7390e6c14e241f05ed
Erfan gharche beydokhti <erfan.erato.79@gmail.com>
2023-02-21 21:12:32 +0330

    complete invoice preview

--- src/components/InvoicePreview.jsx
+++ src/components/InvoicePreview.jsx
@@ -62,26 +62,108 @@ const InvoicePreview = () => {
                 <td>American Bank</td>
               </tr>
               <tr>
-                <td>Country:	</td>
+                <td>Country: </td>
                 <td>$12,110.55</td>
               </tr>
               <tr>
-                <td>IBAN:	</td>
+                <td>IBAN: </td>
                 <td>ETD95476213874685</td>
               </tr>
               <tr>
-                <td>SWIFT Code:	</td>
+                <td>SWIFT Code: </td>
                 <td>BR91905</td>
               </tr>
             </table>
           </div>
-
         </div>
 
         <hr />
-        <div className="">table</div>
-        <div className="invoice-form-resault">resault</div>
-        <div className="invoice-form-footer">footer</div>
+
+        <div className="invoice-form-table">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">ITEM</th>
+                <th scope="col">DESCRIPTION</th>
+                <th scope="col">HOURS</th>
+                <th scope="col">QTY</th>
+                <th scope="col">TOTAL</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Premium Branding Package</th>
+                <td>Branding & Promotion</td>
+                <td>15</td>
+                <td>1</td>
+                <td>$32</td>
+              </tr>
+              <tr>
+                <th scope="row">SMM </th>
+                <td>Social media templates</td>
+                <td>14</td>
+                <td>1</td>
+                <td>$28</td>
+              </tr>
+              <tr>
+                <th scope="row">Web Design</th>
+                <td>Web designing package </td>
+                <td>12</td>
+                <td>1</td>
+                <td>$24</td>
+              </tr>
+              <tr>
+                <th scope="row">SEO</th>
+                <td>Search engine optimization</td>
+                <td>5</td>
+                <td>1</td>
+                <td>$22</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <hr />
+
+        <div className="invoice-form-information">
+          <div className="invoice-to">
+            <div>
+              <p>Salesperson: Jenny Parker</p>
+              <p>Thanks for your business</p>
+            </div>
+          </div>
+
+          <div className="resualt">
+            <table>
+              <tr>
+                <td className="resualt-table">Subtotal: </td>
+                <td>$154.25</td>
+              </tr>
+              <tr>
+                <td>Discount: </td>
+                <td>$00.00</td>
+              </tr>
+              <tr>
+                <td>Tax: </td>
+                <td>$50.00</td>
+              </tr>
+              <tr>
+                <td>Total: </td>
+                <td>$204.25</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+
+        <hr />
+        <div className="invoice-form-footer">
+          <div>
+            Note: <br />
+            It was a pleasure working with you and your team. We hope you will
+            keep us in mind for future freelance projects.
+            <br /> Thank You!
+          </div>
+        </div>
       </div>
       <div className="invoice-operation container">
         <button className="send">SEND INVOICE</button>
